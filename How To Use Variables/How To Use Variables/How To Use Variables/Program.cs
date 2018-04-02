d3628771bcc51cf81a3bc74fd5a69c7882925831
Archesyn <Gdboy1996@live.co.uk>
2018-04-02 14:48:53 +0100

    How To Use Variables ✓

--- How To Use Variables/How To Use Variables/How To Use Variables/Program.cs
+++ How To Use Variables/How To Use Variables/How To Use Variables/Program.cs
@@ -10,6 +10,20 @@ namespace How_To_Use_Variables
     {
         static void Main(string[] args)
         {
+            //A variable is a representation of information, so when you write a programe I would need to store information somewhere so I can access that information and do something with it.
+            //In C# variables have types.
+
+            int countOfBooks = 1;
+            string myName = "George"; //string is a series of characters
+            bool IsCompatible = true; //Bolean can only be true or false
+            string number = "2";
+            int numberconverted = Convert.ToInt32(number);
+
+            Console.WriteLine(countOfBooks);
+            Console.WriteLine(myName);
+            Console.WriteLine(IsCompatible);
+            Console.WriteLine(number);
+            Console.WriteLine(numberconverted);
         }
     }
 }

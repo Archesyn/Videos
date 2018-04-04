097dbb9b82acd14c99dd8818b9a52b0fd8cfe504
Archesyn <Gdboy1996@live.co.uk>
2018-04-04 16:28:21 +0100

    Do While Loop ✓

--- Do While/Do While Loop/Do While Loop/Program.cs
+++ Do While/Do While Loop/Do While Loop/Program.cs
@@ -10,6 +10,16 @@ namespace Do_While_Loop
     {
         static void Main(string[] args)
         {
+            string input = string.Empty;
+            do
+            {
+                input = Console.ReadLine();
+                Console.WriteLine("Your input is {0}.", input);
+            }
+            while (!input.Equals(string.Empty));
+            {
+
+            }
         }
     }
 }

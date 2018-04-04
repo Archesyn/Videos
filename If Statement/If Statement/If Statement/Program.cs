2bb6c984418bdf42ab0ef502ba84a03f6fbb4e72
Archesyn <Gdboy1996@live.co.uk>
2018-04-04 14:45:32 +0100

    If Statement/Control Flow ✓

--- If Statement/If Statement/If Statement/Program.cs
+++ If Statement/If Statement/If Statement/Program.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//ControlFlowDecision
 namespace If_Statement
 {
     class Program
     {
         static void Main(string[] args)
         {
+           string player =  Console.ReadLine();
+
+            if(player.Equals("AI"))
+            {
+                Console.WriteLine("The player is AI.");
+            }
+            else if (player.Equals("Human"))
+            {
+                Console.WriteLine("The player is Human.");
+            }
+            else
+            {
+                Console.WriteLine("The player is not AI and not Human");
+            }
         }
     }
 }

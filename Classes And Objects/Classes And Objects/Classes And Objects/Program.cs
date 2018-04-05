3685705306bdce749ab0eceb1fb798b30617be9f
Archesyn <Gdboy1996@live.co.uk>
2018-04-05 15:10:28 +0100

    Classes And Objects ✓

---
+++ Classes And Objects/Classes And Objects/Classes And Objects/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_And_Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            Person person2 = new Person();
+            // "new" it used to create the object based on the class, and then the "=" operator is used to assign the object to a variable "person1".
+            //Each new object of that class is a new instance of that class.
+        }
+    }
+}

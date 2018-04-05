521372902de8bc4c9aaccd3dc189068452970034
Archesyn <Gdboy1996@live.co.uk>
2018-04-05 15:59:24 +0100

    Methods ✓

---
+++ Methods/Methods/Methods/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    public class Person
+    {
+        public string GetFullName(string Firstname, string Lastname)
+        {
+            return $"The fullname of the person is {Firstname} {Lastname}";
+        }
+    }
+}

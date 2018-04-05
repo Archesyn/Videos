2de3d91c2dff54ddc101a8cbf949165fa0afaca5
Archesyn <Gdboy1996@live.co.uk>
2018-04-05 15:26:27 +0100

    Object Oriented Programming Basics ✓

---
+++ Object Oriented Programming Basics/ObjectOrientedProgrameBasics/ObjectOrientedProgrameBasics/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedProgrameBasics
+{
+    public class Person
+    {
+        public string FirstName { set; get; }
+        public string Lastname { set; get; }
+
+        public void GetFullName()
+        {
+            Console.WriteLine("Get my fullname {0} {1}.", FirstName, Lastname);
+        }
+
+        public void Walk()
+        {
+            Console.WriteLine("I am walking.");
+        }
+
+        public void Talk()
+        {
+            Console.WriteLine("I am talking.");
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine("I am eating.");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine("I am sleeping.");
+        }
+    }
+}
--- Object Oriented Programming Basics/ObjectOrientedProgrameBasics/ObjectOrientedProgrameBasics/Program.cs
+++ Object Oriented Programming Basics/ObjectOrientedProgrameBasics/ObjectOrientedProgrameBasics/Program.cs
@@ -10,6 +10,35 @@ namespace ObjectOrientedProgrameBasics
     {
         static void Main(string[] args)
         {
+            Person me = new Person();
+            me.FirstName = "George";
+            me.Lastname = "Dennis";
+
+            me.GetFullName();
+
+            me.Walk();
+            me.Eat();
+            me.Talk();
+            me.Sleep();
+
+            SuperPerson superPerson = new SuperPerson();
+            superPerson.FirstName = "Super";
+            superPerson.Lastname = "Dude";
+            superPerson.GetFullName();
+
+            superPerson.Walk();
+            superPerson.Eat();
+            superPerson.Talk();
+            superPerson.Sleep();
+            superPerson.Fly();
+
+
+
+
+
+
+
+
         }
     }
 }

f3f63c5c2d35008567084164e00ce8652556495a
Archesyn <Gdboy1996@live.co.uk>
2018-04-07 15:39:12 +0100

    Methods With a Void Return Type ✓

---
+++ Methods With Void Return Type/Methods With Void Return Types/Methods With Void Return Types/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_With_Void_Return_Types
+{
+    class Person
+    {
+        public void GetFullName (string Firstname, string Lastname)
+        {
+            Console.WriteLine($"The fullname of the person is {Firstname} {Lastname}");
+        }
+    }
+}
---
+++ Methods With Void Return Type/Methods With Void Return Types/Methods With Void Return Types/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_With_Void_Return_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.GetFullName("George", "Dennis");
+        }
+    }
+}

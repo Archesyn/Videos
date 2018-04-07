2c991f74b7e0527937500a4f36d5acf434230e8f
Archesyn <Gdboy1996@live.co.uk>
2018-04-07 16:18:07 +0100

    Properties ✓

---
+++ Properties/Properties/Properties/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+            player.Lives = 100;
+            Console.WriteLine(player.Lives);
+        }
+    }
+}
